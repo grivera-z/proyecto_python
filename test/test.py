8270bb9072804e6952e16419ce88b7cf06fcbfb2
Gabriel Rivera <gabriel.rivera.cib@gmail.com>
2018-07-17 23:14:35 -0500

    add test

--- test/test.py
+++ test/test.py
@@ -1,4 +1,12 @@
-f= open("test.txt","w+")
-for i in range(10):
-     f.write("This is line %d\r\n" % (i+1))
-f.close() 
+# f= open("test.txt","w+")
+# for i in range(10):
+#      f.write("This is line %d\r\n" % (i+1))
+# f.close() 
+
+aux = "holaZEO"
+
+s = "This be a string"
+if aux.upper().find("ZEO") == -1:
+    print("No 'is' here!")
+else:
+    print ("Found 'is' in the string.")
